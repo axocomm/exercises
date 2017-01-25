efd44ee73c43e8ae2287365aeda8725307cfcb6b
axocomm <axocomm@gmail.com>
2017-01-24 22:07:02 -0500

    Start recursive helper function and applyUpdate

--- update/index.js
+++ update/index.js
@@ -1,21 +1,76 @@
 'use strict';
 
 var Commands = {
-  $push: function (target, item) {
-    return target.concat(item);
-  },
-
-  $set: function (target, key, value) {
-    target[key] = value;
-    return target;
+  $push: function (item) {
+    return this.concat(item);
   }
 };
 
-function update(state, updates) {
-  return Commands.$push(state, updates);
+function d(msg) {
+  console.log('=== ' + msg);
 }
 
-console.log(update([1], [2]));
-console.log(Commands.$set({abc: 'def'}, 'abc', 'fgh'));
+/**
+ * Determine if the given object is a state update command.
+ *
+ * @param {object} [update] the current update
+ *
+ * @return {boolean} if the "node" is a command
+ */
+function isCommand(update) {
+  var keys = Object.keys(update);
+  return keys.length === 1 && keys[0] in Commands;
+}
+
+/**
+ * Apply an update to the given state.
+ *
+ * @param {any}    [state]  the state
+ * @param {object} [update] the update to perform
+ *
+ * @return {any} the transformed state
+ */
+function applyUpdate(update, state) {
+  var k = Object.keys(update)[0];
+  var arg = update[k];
+  var commandFn = Commands[k];
+
+  d('State is ' + JSON.stringify(state));
+  d('Update is ' + JSON.stringify(update));
+  d('Arg is ' + JSON.stringify(arg));
+
+  if (typeof commandFn === 'undefined') {
+    throw("Invalid command '" + k + "'");
+  }
+
+  return commandFn.call(state, arg);
+}
+
+/**
+ * Perform updates on the given state.
+ *
+ * Utilizes a helper function to recursively call itself
+ * for all keys in `state` until a command ("leaf") node is
+ * found, at which point the transformation is applied to the
+ * current location in `state`.
+ *
+ * @param {any}    [state]   the initial state
+ * @param {object} [updates] the intended updates
+ *
+ * @return {any} the state after transformation
+ */
+function update(state, updates) {
+  var helper = function (s, u) {
+    if (isCommand(u)) {
+      return applyUpdate(u, s);
+    }
+  };
+
+  return helper(state, updates);
+}
+
+var state = [1, 2];
+var updates = {$push: [3]};
+console.log(update(state, updates));
 
 module.exports = update;
